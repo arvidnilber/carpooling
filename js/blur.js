9936300fd5adf1aae29f9c9bf6e19599cc99bb1a
Arvid <aanilber@gmail.com>
2018-03-06 13:55:28 +0100

    Changed some stuff

--- js/blur.js
+++ js/blur.js
@@ -7,14 +7,12 @@ $(function() {
       $('#blur-overlay').addClass('blur-in');
       $('#blur-overlay').removeClass('blur-out');
       e.stopPropagation();					 
-  }); 
-	
+  });
       $('.close-button').click(function (e) { 
 
       $('#swish').fadeOut(700);
       $('#blur-overlay').removeClass('blur-in');
       $('#blur-overlay').addClass('blur-out');
       e.stopPropagation();
-        
     });
  });
